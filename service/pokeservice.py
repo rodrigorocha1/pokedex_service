d5cbdedb175d3958ac037a0e3b2b663fb7731f5f
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-11-21 22:02:01 -0300

    classe para fazer requisição do pokemon

---
+++ service/pokeservice.py
@@ -0,0 +1,31 @@
+import requests
+from requests import exceptions as ex
+from entidades.pokemon import Pokemom
+
+
+class PokeAPI:
+    def __init__(self):
+        self.__base_url = 'https://pokeapi.co/api/'
+
+    def obter_dados_pokemon_id(self, id_pokemon: int):
+        url = f'{self.__base_url}/v2/pokemon/{id_pokemon}/'
+        try:
+            req = requests.get(url)
+
+            pokemon = Pokemom(**req.json())
+            return pokemon
+        except ex.HTTPError as errh:
+            print("Http Error:", errh)
+        except ex.ConnectionError as errc:
+            print("Error Connecting:", errc)
+        except ex.Timeout as errt:
+            print("Timeout Error:", errt)
+        except ex.RequestException as err:
+            print("OOps: Something Else", err)
+        except Exception as e:
+            print(e)
+
+
+
+
+
