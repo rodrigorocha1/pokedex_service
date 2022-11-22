61ba385142385cf1e39285b7e2fe526eb0832bad
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-11-21 21:52:39 -0300

    Classe para deserializar o json da requisição da api

--- entidades/pokemon.py
+++ entidades/pokemon.py
@@ -3,21 +3,41 @@ from enuns.pokeenuns import Cor
 
 class Pokemom:
     def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.id = kwargs['id']
-        self.tipos = [tipos['type']['name'] for tipos in kwargs.get('types')]
-        self.cor = ''.join(
+        self._name = kwargs['name']
+        self._id = kwargs['id']
+        self._tipos = [tipos['type']['name'] for tipos in kwargs.get('types')]
+        self._cor = ''.join(
             [cor.value for cor in Cor
              if [tipos['type']['name']
                  for tipos in kwargs.get('types')][0] in cor.name])
-        self.habilidade = [habilidades['ability']['name'] for habilidades in kwargs['abilities']]
-        self.estatisicas = [
+        self._habilidade = [habilidades['ability']['name'] for habilidades in kwargs['abilities']]
+        self._estatisicas = [
             {
-                'nome': j['stats'][chave]['stat']['name'],
-                'valor': j['stats'][chave]['base_stat']
+                'nome': kwargs['stats'][chave]['stat']['name'],
+                'valor': kwargs['stats'][chave]['base_stat']
             } for chave, _ in enumerate(kwargs['stats'])
         ]
 
-    def __str__(self):
-        stc = self.img, self.tipos, self.nome, self.id
-        return str(stc)
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def tipos(self):
+        return self._tipos
+
+    @property
+    def cor(self):
+        return self._cor
+
+    @property
+    def habilidade(self):
+        return self._habilidade
+
+    @property
+    def estatisticas(self):
+        return self._estatisicas
